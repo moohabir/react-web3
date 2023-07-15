05fe00e2f0dacdd3df39d2dd9ddfa087d5ea9755
moohabir <moohabir@gmail.com>
2023-07-15 21:29:25 +0200

    modified:   .gitignore         modified:   contract/truffle-config.js

--- contract/truffle-config.js
+++ contract/truffle-config.js
@@ -1,48 +1,38 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-const mnemonic =
-  'candy maple cake sugar pudding cream honey rich smooth crumble sweet treat';
+const mnemonic = process.env.REACT_APP_MEMONIC;
 
 module.exports = {
   networks: {
     development: {
-      host: '127.0.0.1',
-      port: 7545,
-      network_id: '5777',
+      host: process.env.REACT_APP_HOST,
+      port: process.env.REACT_APP_PORT,
+      network_id: process.env.REACT_APP_NETWORK_ID,
     },
     //truffle migrate --network ganache for to deploy to the ethereum ganache local test
     ganache: {
-      host: '127.0.0.1',
-      port: 7545,
-      network_id: '5777',
+      host: process.env.REACT_APP_HOST,
+      port: process.env.REACT_APP_PORT,
+      network_id: process.env.REACT_APP_NETWORK_ID,
     },
     //truffle migrate --network rinkeby for to deploy to the ethereum rinkeby testnet
     rinkeby: {
       provider: () =>
-        new HDWalletProvider(
-          mnemonic,
-          'https://rinkeby.infura.io/v3/05cf6524087d4ee185185808c234a891'
-        ),
+        new HDWalletProvider(mnemonic, process.env.REACT_APP_RINKEBY),
       network_id: 4,
       gas: 5500000,
     },
 
     goerli: {
       provider: () =>
-        new HDWalletProvider(
-          mnemonic,
-          'https://gocerli.infura.io/v3/05cf6524087d4ee185185808c234a891'
-        ),
-      //network_id: 4, change and find the 
+        new HDWalletProvider(mnemonic, process.env.REACT_APP_GOERLI),
+      //network_id: 4, change and find the
       gas: 5500000,
     },
     //truffle migrate --network mainnet for to deploy to the ethereum mainnnet not test
 
     mainnet: {
       provider: () =>
-        new HDWalletProvider(
-          mnemonic,
-          'https://mainnet.infura.io/v3/05cf6524087d4ee185185808c234a891'
-        ),
+        new HDWalletProvider(mnemonic, process.env.REACT_APP_MAINET),
       network_id: 1,
       gas: 7500000,
       gasPrice: 1000000000,
